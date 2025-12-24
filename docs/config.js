7574d7953e2039c380fb2404cbdadafdaaf122ab
nyamdavaa528 <fiance0408@gmail.com>
2025-12-24 15:34:33 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,5 @@
 // docs/config.js
 // Example: "https://poker-table-backend.onrender.com"
-window.SOCKET_URL = "https://poker-tlfv.onrender.com";
+window.SOCKET_URL = "https://poker-1-dzj9.onrender.com";
+
 
